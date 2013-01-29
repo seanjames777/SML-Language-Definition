73a62635be46bdafe03cb6f17054d8326dac45d1
Sean James <seanjames777@gmail.com>
2013-01-28 20:40:25 -0500

    Add a script to automatically find SML on macs regardless of version number

---
+++ sml_mac.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+COMMON_LOCATIONS="/opt/local/bin:/usr/bin:/opt/local/sbin:/bin:/usr/sbin:/usr/local/bin"
+SEARCH=`find /usr/local/smlnj*/bin -type d -maxdepth 0 | sort -nr | head -n1`
+
+PATH=$SEARCH:$COMMON_LOCATIONS:$PATH
+
+sml $1
